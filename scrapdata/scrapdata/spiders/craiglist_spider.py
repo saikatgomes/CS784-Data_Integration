b29b09c4ba00001e87fd77a5c6c39a47fd7385a7
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-10-07 18:34:33 -0500

    scrap deleted

--- scrapdata/scrapdata/spiders/craiglist_spider.py
+++ scrapdata/scrapdata/spiders/craiglist_spider.py
@@ -1,15 +1,26 @@
 import scrapy
+from scrapy.http import Request
 
 class CraglistSpider(scrapy.Spider):
     name = "craiglist"
-    allowed_domains = ["*.craigslist.org"]
+    allowed_domains = ["madison.craigslist.org"]
     start_urls = [
-        "http://madison.craigslist.org/cto/"
+        "http://madison.craigslist.org/search/cta"
     ]
-
+    link_list = []
     def parse(self, response):
-        for sel in response.xpath('//p[/li'):
-            title = sel.xpath('a/text()').extract()
+        link_list = self.link_list
+        already_done = []
+        for sel in response.xpath('//p[@class="row"]'):
             link = sel.xpath('a/@href').extract()
-            desc = sel.xpath('text()').extract()
-            print title, link, desc
+            link_list.append(link)
+            url = response.url
+            if url.find("?s=")<0:
+                url = "%s?s=0" % url
+            url.split('?s=')
+            u = url.split('?s=')[1]
+            re.sub('
+            if c<1000:
+                url = url.replace(str(c), str(c+100))
+                yield Request(self.start_urls[0]+"?s=100&")
+                
