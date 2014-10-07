44cffaf18764b1e24e6c79f0b4eb2f657a0a7d71
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-10-07 13:03:15 -0500

    removed scrappy folder form git

---
+++ scrapdata/scrapdata/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapdata project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapdata'
+
+SPIDER_MODULES = ['scrapdata.spiders']
+NEWSPIDER_MODULE = 'scrapdata.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapdata (+http://www.yourdomain.com)'
---
+++ scrapdata/scrapdata/spiders/craiglist_spider.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class CraglistSpider(scrapy.Spider):
+    name = "craiglist"
+    allowed_domains = ["*.craigslist.org"]
+    start_urls = [
+        "http://madison.craigslist.org/cto/"
+    ]
+
+    def parse(self, response):
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print title, link, desc
