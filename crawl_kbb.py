d1c528e852c99425e68c16cece5ec307e130f949
saikatgomes <saikatgomes@gmail.com>
2014-10-08 03:30:05 -0500

    KBB: company>category>model>year

--- crawl_kbb.py
+++ crawl_kbb.py
@@ -9,6 +9,7 @@ used = "/used-cars/"
 D =[]
 D2 =[]
 D3 =[]
+D4 =[]
 
 # current time for logging
 def getTime(f=1):
@@ -53,6 +54,7 @@ def get_category():
     makes=X.get('makes')
     url=X.get('url')
     for x in range(0,len(url)):
+    #for x in range(0,1):
         newUrl=base_url+url[x]
         print(getTime(1)+"|>> crawling for categories of "+makes[x]+" ["+newUrl+"]")
         new_page=requests.get(newUrl)
@@ -71,6 +73,7 @@ def get_model():
     global D2
     print(getTime(1)+"|> fetching car models ...")
     for x in range(0,len(D2)):
+    #for x in range(0,1):
         X=D2[x]
         c_name=X.get('company')
         cat=X.get('cat')
@@ -87,10 +90,34 @@ def get_model():
                 model.extend(a.xpath('a/text()'))
                 model_url.extend(a.xpath('a/@href'))
             D3.append({'company':c_name,'cat':cat[y],'model':model,'url':model_url})
-            print '\n'.join("%s -->  %s" %(a,b) for a,b in zip(model,model_url)) 
+            #print '\n'.join("%s -->  %s" %(a,b) for a,b in zip(model,model_url)) 
             print '\n              |>>>> '.join(model)
             time.sleep(1)
 
+def get_year():
+    global D3
+    print(getTime(1)+"|> fetching car years ...")
+    for x in range(0,len(D3)):
+        X=D3[x]
+        c_name=X.get('company')
+        cat=X.get('cat')
+        model=X.get('model')
+        url=X.get('url')
+        print(getTime(1)+"|>> crawling for company="+c_name+" category="+cat)
+        for y in range(0,len(model)):
+            newUrl=base_url+url[y]
+            print(getTime(1)+"|>>> crawling for company="+c_name+" category="+cat+" model="+model[y]+" ["+url[y]+"]")
+            new_page=requests.get(newUrl)
+            tree=html.fromstring(new_page.text)
+            year = []
+            year_url = []
+            for a in tree.xpath("//div[@class='model-year']/div/div[@class='left']"):
+                year.extend(a.xpath("span[@class='model-year-name']/text()"))
+                year_url.extend(a.xpath("a[@class='section-title']/@href"))
+            D4.append({'company':c_name,'cat':cat,'model':model[y],'year':year,'url':year_url})
+            #print '\n'.join("%s -->  %s" %(a,b) for a,b in zip(year,year_url))
+            print '\n              |>>>> '.join(year)
+            time.sleep(1)
 
 if __name__ == "__main__":
     get_all_makes(base_url,"new")
@@ -107,4 +134,8 @@ if __name__ == "__main__":
     fileName = 'data/kbb_dict3_'+ts+'.json'
     with open(fileName,"w") as f3:
         json.dump({"car_cat_model_link": D3},f3,indent=2)
+    get_year()
+    fileName = 'data/kbb_dict4_'+ts+'.json'
+    with open(fileName,"w") as f4:
+        json.dump({"car_cat_model_year_link": D4},f4,indent=2)
 
