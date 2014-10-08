f95ab1cfd0be72487b03b34ada4af4db67eac19b
saikatgomes <saikatgomes@gmail.com>
2014-10-07 23:31:47 -0500

    config file

--- crawl_kbb.py
+++ crawl_kbb.py
@@ -2,36 +2,33 @@
 
 import requests
 from lxml import html
-import json, time
+import json, time, datetime
 
 base_url = "http://www.kbb.com"
 used = "/used-cars/"
 D =[]
-#base_search_url = base_url + "/search/cta"
 
-'''
-def get_all_links(url, limmit_hop = 100):
-    first_page = requests.get(url)
-    tree = html.fromstring(first_page.text)
-    links = []
-    links.extend(tree.xpath("//p[@class='row']/a/@href"))
-    for i in xrange(1,limmit_hop):
-        first_page = requests.get(url+"?s=%d&" % (i*100))
-        tree = html.fromstring(first_page.text)
-        links.extend(tree.xpath("//p[@class='row']/a/@href"))
-        time.sleep(1)
-    print links
 
-def car_info(url):
-    page = request.get(url)
-    tree = html.fromstring(page)
-    _id_ = re.match(".*(\d+)\.html")
-    c = {"id": }
-'''
+# current time for logging
+def getTime(f=1):
+    ts = time.time()
+    fmt=""
+    if f==1:
+        fmt='%Y-%m-%d--%H-%M-%S--%f'
+    else:
+        fmt='%Y%m%d%H%M%S%f'
+    dt = datetime.datetime.fromtimestamp(ts).strftime(fmt)
+    return dt
 
+# current date for logging
+def getDate():
+    ts = time.time()
+    dt = datetime.datetime.fromtimestamp(ts).strftime('%d%m%Y')
+    return dt
 
 def get_all_makes(url,tp):
     global D
+    print(getTime(1)+" fetching makes for "+tp) 
     main_page=requests.get(url)
     tree = html.fromstring(main_page.text)
     car_makes = []
@@ -39,14 +36,18 @@ def get_all_makes(url,tp):
     for a in tree.xpath("//ul[@class='contentlist by-make']/li"):
         car_makes.extend(a.xpath('a/text()'))
         car_makes_links.extend(a.xpath('a/@href'))
-    print '\n'.join("%s -->  %s" %(a,b) for a,b in zip(car_makes,car_makes_links))
+    #print '\n'.join("%s -->  %s" %(a,b) for a,b in zip(car_makes,car_makes_links))
     D.append({"makes":car_makes,"type":tp,"url":car_makes_links})
+    time.sleep(1)
+
+
 
 
 if __name__ == "__main__":
     get_all_makes(base_url,"new")
     get_all_makes(base_url+used,"used");
-    with open("foo.json","w") as f:
+    fileName = 'data/kbb_dict_'+getTime()+'.json'
+    with open(fileName,"w") as f:
         json.dump({"car_makes_link": D},f,indent=2)
 
 
