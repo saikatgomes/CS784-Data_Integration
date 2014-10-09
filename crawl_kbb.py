ba1c283983db21ffa632a1f2d5095b0095233f07
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-10-09 11:06:06 -0500

    ~~

--- crawl_kbb.py
+++ crawl_kbb.py
@@ -4,15 +4,16 @@ import requests
 from lxml import html
 import json, time, datetime, random
 from collections import defaultdict
+import itertools, os, sys
 
 base_url = "http://www.kbb.com"
 used = "/used-cars/"
 D = defaultdict(dict)
+kbb_dump_filename = "kbb_cars_data.json"
 
-D2 = {}
-D3 =[]
-D4 =[]
-
+# TESTING PURPOSE
+lim = 2
+# make -> [new/used] -> category -> model -> year
 # current time for logging
 def getTime(f=1):
     ts = time.time()
@@ -37,36 +38,81 @@ def sanity_check():
         for key in keys_essentials:
             if key not in v:
                 print "Key '%s' not for %s" % (key, k)
+    return
 
-def get_all_makes(url,tp):
-    global D
-    print(getTime(1)+"|> fetching makes for "+tp) 
+parse_dict = {
+    'make' : ["//ul[@class='contentlist by-make']/li"],
+    'category': ["//ul[@class='browse-category']/li/span"],
+    'model': ["//div[@id='Make-category']/div[@class='collapse']/div/div/span[@class='left']"],
+    'year' : ["//div[@class='model-year']/div/div[@class='left']", 
+              "span[@class='model-year-name']/text()", 
+              "a[@class='section-title']/@href"
+              ]
+    }
+level = ['make', 'category', 'model', 'year']
+    
+def parse_page(url, xpath_head, xpath_values = ['a/text()', 'a/@href']):
     main_page=requests.get(url)
     tree = html.fromstring(main_page.text)
-    # car_makes_and_links = {}
-    # car_makes_links = []
-    for a in tree.xpath("//ul[@class='contentlist by-make']/li"):
-        for m,l in zip (a.xpath('a/text()'),
-                        a.xpath('a/@href')):
-            D[m][tp] = l
+    ret = []
+    for a in tree.xpath(xpath_head):
+        ret.extend(zip(*(a.xpath(v) for v in xpath_values)))
+    return ret[:lim]
+
+
+def is_url(url):
+    return type(url) == str
+
+def start_getting_data(fname, keys=[]):
+    global D
+    if os.path.exists(fname):
+        D = json.load(open(fname))
+    else: 
+        #get_all_makes(base_url,"new")
+        get_all_makes(base_url+used,"used");
+    print json.dumps(D, indent=4, sort_keys=True)
+
+    for k in keys:
+        print "Trying for:", k 
+        if k not in D: continue
+
+        D[k] = get_all_data(D[k], l=1)
+    json.dump(D, open(fname, 'wb'), 
+              indent=True, sort_keys=True)
+
+
+def get_all_data(d, l):
+    if l>=len(level): return
+    typ = level[l]
+    if type(d) == str:
+        return dict(parse_page(base_url+d, parse_dict[typ][0],
+                               parse_dict[typ][1:]))
+    for k,v in d.items():
+        if type(v) == str:
+            d[k] = dict(parse_page(base_url+v, parse_dict[typ][0],
+                                   parse_dict[typ][1:]))
+            get_all_data(d[k], l+1)
+    return 
+
+def get_all_makes(url,tp="used"):
+    if tp!='used':
+        print "Probably I dont care about 'new' cars!"
+        return
+    global D
+    print(getTime(1)+"|> fetching makes for "+tp) 
+    for m,l in parse_page(url, "//ul[@class='contentlist by-make']/li"):
+        D[m] = l
     time.sleep(1)
 
 def get_category():
     global D
     print(getTime(1)+"|> fetching car categories ...") 
-    #THIS IS SHIT
-    #print '\n'.join("%s -->  %s" %(a,b) for a,b in zip(X.get('makes'),X.get('url'))) 
     for make,values in D.items():
-    #for x in range(0,1):
         u = values.get('used', '')
         if not u: continue
         newUrl=base_url + u
         print(getTime(1)+"|>> crawling for categories of "+ make + " ["+newUrl+"]")
-        new_page=requests.get(newUrl)
-        tree = html.fromstring(new_page.text)
-        values['categories'] = {}
-        for a in tree.xpath("//ul[@class='browse-category']/li/span"):
-            values['categories'] = dict(zip(a.xpath('a/text()'), a.xpath('a/@href')))
+        values['categories'] = dict(parse_page(newUrl, "//ul[@class='browse-category']/li/span"))
         if random.randint(0,10)>8:
             time.sleep(1)
 
@@ -75,12 +121,15 @@ def get_model():
     print(getTime(1)+"|> fetching car models ...")
     for make, v in D.items():
         print(getTime(1)+"|>> crawling for models of "+make)
-        for cat, cat_url in v['categories'].items():
+        categories = v.get('categories', '')
+        if not categories:
+            print "'categories' not found in --> ", make, ':', v
+            continue
+        for cat, cat_url in categories.items():
             newUrl=base_url+ cat_url
             print(getTime(1)+"|>>> crawling for models of "+make+" category="+cat+" ["+cat_url+"]")
             new_page=requests.get(newUrl)
             tree=html.fromstring(new_page.text)
-            cat_dict = {}
             for a in tree.xpath("//div[@id='Make-category']/div[@class='collapse']/div/div/span[@class='left']"):
                             v['categories'][cat] = dict(zip(a.xpath('a/text()'), a.xpath('a/@href')))
         if random.randint(0,10)>8:
@@ -112,26 +161,4 @@ def get_year():
             time.sleep(1)
 
 if __name__ == "__main__":
-    get_all_makes(base_url,"new")
-    get_all_makes(base_url+used,"used");
-    #print json.dumps(D, indent=2, sort_keys= True)
-    sanity_check()
-    ts=getTime()
-    fileName = 'data/kbb_dict.json'
-    with open(fileName,"w") as f:
-        json.dump(D,f,indent=2)
-    get_category()
-    get_model()
-    print json.dumps(D, indent=2, sort_keys= True)
-    # fileName = 'data/kbb_dict2_'+ts+'.json'
-    # with open(fileName,"w") as f2:
-    #     json.dump({"car_cat_link": D2},f2,indent=2)
-    # get_model()
-    # fileName = 'data/kbb_dict3_'+ts+'.json'
-    # with open(fileName,"w") as f3:
-    #     json.dump({"car_cat_model_link": D3},f3,indent=2)
-    # get_year()
-    # fileName = 'data/kbb_dict4_'+ts+'.json'
-    # with open(fileName,"w") as f4:
-    #     json.dump({"car_cat_model_year_link": D4},f4,indent=2)
-
+    start_getting_data(kbb_dump_filename, ['Acura'])
