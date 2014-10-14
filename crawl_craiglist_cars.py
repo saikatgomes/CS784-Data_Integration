9d0afb60a1c5c8784831399c135a81ed60fe1f1c
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-10-14 04:40:24 -0500

    Adding 5k craiglist pages rom Madison and Milwuakee

--- crawl_craiglist_cars.py
+++ crawl_craiglist_cars.py
@@ -4,47 +4,46 @@
 import requests
 from lxml import html
 import json, time, re
-import shutil
+import shutil, sys, os
+import random
 
-base_url = "http://madison.craigslist.org"
-base_search_url = base_url + "/search/cta"
+cities = ['madison', 'milwaukee']
+base_urls = ["http://%s.craigslist.org" % s for s in cities]
 IMGDIR = 'car_images/'
 HTMLDIR = 'craiglist/html_craiglist/'
 
 cars = []
-def get_all_links(url, limmit_hop = 100, fromFile=None):
+def get_all_links(city='madison', limmit_hop = 100, fromFile=None):
+    base_url = "http://%s.craiglist.org" % city
+    url = "%s/search/cta" % base_url
     if fromFile:
         with open(fromFile) as f:
             links = [x.strip() for x in f.readlines()]
             return links
-    first_page = requests.get(url)
-    tree = html.fromstring(first_page.text)
     links = []
-    links.extend(tree.xpath("//p[@class='row']/a/@href"))
     last_len = 0
-    for i in xrange(1,limmit_hop+1):
+    for i in xrange(limmit_hop):
         full_url = url+"?s=%d&" % (i*100)
         first_page = requests.get(full_url)
         tree = html.fromstring(first_page.text)
-        links.extend(tree.xpath("//p[@class='row']/a/@href"))
+        links.extend("%s/%s" % (base_url, l)
+                     for l in tree.xpath("//p[@class='row']/a/@href"))
         print "Hop count:", i, full_url
-        if last_len == links:
+        if last_len == len(links):
             break # probably done with crawling the links
         last_len = len(links)
-        time.sleep(1)
-    print links
-    with open('list_links.txt', 'w') as f:
-        f.write('\n'.join(links))
+        if random.randint(0,10)<2:
+            time.sleep(1)
+    print len(links)
     return links
 
 def get_car_info(links):
-    for i,l in enumerate(links):
-        url = "%s%s" % ( base_url, l)
+    for i,url in enumerate(links):
         print "Downloading:", url
         if i%100==0:
-            print ">>>>>>   Done processing %d cars!!!" 
+            print ">>>>>>   Done processing %d cars!!!" % i 
         car_info(url)
-        if i%5==0:
+        if i%10==0:
             time.sleep(1)
     with open('car_data.json', 'wb') as fp:
         json.dump(cars, fp, indent=2)
@@ -53,43 +52,37 @@ def car_info(url):
     global cars
     page = requests.get(url)
     tree = html.fromstring(page.text)
-    _id_ = re.match(r".*/(\d+)\.html", url).group(1)
+    m = re.match(r"http://(?P<city>\w+)\..*/(?P<id>\d+)\.html", url)
+    if m:
+        m = m.groupdict()
+        _id_, city = m['id'], m['city']
+        fname = "%s_%s.html" % (city, _id_)
+        print "Writing:", fname
+        with open("%s/%s.html" % ( HTMLDIR, fname), 'w') as f:
+            f.write(page.text.encode('ascii', errors='ignore'))
+    else:
+        return None
     try:
         title = tree.xpath("//h2[@class='postingtitle']/text()")[1].strip()
+        title = title.encode('ascii', errors='ignore')
     except IndexError:
         return None
     c = {"id": _id_, "title": title}
-    title_parse = re.compile(r"(\d{4})(.*)\$(\d+) \((.*)\)")
-    try:
-        c["year"], c["make"], c["cost"], c["location"] = title_parse.match(title).groups()
-    except AttributeError:
-        c["year"], c["make"], c["cost"], c["location"] = "", "", "", ""
+    c["make"], c["cost"], c["location"], c["year"] = parse_title(title)
 
-    attr = {}
-    for attrib in tree.xpath("//p[@class='attrgroup']/span/text()"):
-        k = attrib.split(':')
-        if len(k)<2:
-            k,v = 'title', k[0]
-        else:
-            k, v = k[0], ','.join(k[1:])
-        v = v.strip()
-        k = k.strip()
-        attr[k] = v
+    attr = extract_atrribute(tree)
     c['attr'] = attr
     c['body'] = ''.join(tree.xpath('//section[@id="postingbody"]/text()'))
     # get images
-    c['img'] = []
-    for img in tree.xpath("//div[@id='thumbs']/a"):
-        imgid = img.xpath('@data-imgid')[0]
-        fname = imgid + ".jpg"
-        # l = img.xpath('@href')[0]
-        # with open(IMGDIR+fname, 'wb') as f:
-        #     shutil.copyfileobj(requests.get(l, stream=True).raw, f)
-        c['img'].append(fname)
+    # c['img'] = []
+    # for img in tree.xpath("//div[@id='thumbs']/a"):
+    #     imgid = img.xpath('@data-imgid')[0]
+    #     fname = imgid + ".jpg"
+    #     # l = img.xpath('@href')[0]
+    #     # with open(IMGDIR+fname, 'wb') as f:
+    #     #     shutil.copyfileobj(requests.get(l, stream=True).raw, f)
+    #     c['img'].append(fname)
 
-    for k in c.keys():
-        if type(c[k]) == str:
-            c[k] = c[k].strip()
     cars.append(c)
 
 def parse_title(s):
@@ -169,12 +162,15 @@ def flatten_table(fname):
             
 
 if __name__ == "__main__":
-    # links = get_all_links(base_search_url, 100, fromFile='list_links.txt')
+    # links = get_all_links(cities[0], 100, fromFile='list_links.txt')
+    # links.extend(get_all_links(cities[1], 100))
+    # with open('list_links.txt', 'w') as f:
+    #     f.write('\n'.join(links))
     # print links
     # get_car_info(links)
     # print parse_title(u"\u2606\u2606 1990 CADILLAC SEDAN DEVILLE \u2606\u2606 - $1200 (Marshall, Wi 53559)")
-    #with open(HTMLDIR + '4704328594.html') as f:
+    # with open(HTMLDIR + '4704328594.html') as f:
     #    tree = html.fromstring(f.read())
     #    print json.dumps(extract_atrribute(tree), indent=4)
     # clean_car_json_data('craiglist/car_data.json')
-    flatten_table('craiglist/car_data_new.json')
+    flatten_table(sys.argv[1])
