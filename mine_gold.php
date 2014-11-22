b6fddcbf8ba8f51edd9bec97731c25019ee51d93
Rahul Chatterjee <rchatterjee4@wisc.edu>
2014-11-22 10:27:40 -0600

    Some more modification. TODO: subset of keys to show

--- mine_gold.php
+++ mine_gold.php
@@ -18,14 +18,25 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <meta charset='UTF-8'>
 <title>Golden data</title>
 <html>
-  <header> <h2>Randomly generated ids from KBB and Craiglist to match</h2></header>
+  <header> 
+    <style>
+     table, td, tr {
+       border: 1px solid black;
+       }
+     td.kbb, td.craiglist {
+       width: 400px;
+     }
+    </style>
+    <h2>Randomly generated ids from KBB and Craiglist to match</h2>
+  </header>
   <script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
-  <body>
+  <body align="center">
     <?php    
     $myfile = file("data/blocking_data/blocks.table") or die("Unable to open file!");
     $total_sample = 0;
     $kbb_t = json_decode(file_get_contents("data/all_makes_table_kbb.json"), true)['table']['tuples'] or die("Cannot load kbb data");
     $craiglist_t = json_decode(file_get_contents("data/all_makes_table_craiglist.json"), true)['table']['tuples'];
+    $gold = json_decode(file_get_contents($gold_file), true);
     $kbb = array();
     $craiglist = array();
     
@@ -36,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
       $craiglist[$t['id']] = $t;
     }
 
-    echo "<table border='1'>";
+    echo "<table class='data' border='1'>";
     echo "<tr class='header'><td>KBB</td><td>Craiglist</td><td>Match</td></tr>";
     foreach ($myfile as $line_num => $line) {
       if ($line_num ==0) continue;
@@ -45,21 +56,29 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$total_sample += 1 ;
 	#echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
 	list($id1, $id2, $id3) = split(',', $line);
-	echo "<td>";
+	echo "<td class='kbb'>";
 	# selected.append($id1);
+
+	# TODO use some subset of keys to match
+	$useful_kbb_key = array_keys($kbb_t);#[];
+	$useful_craig_key = array_keys($craiglist_t);#[];
+    
 	foreach($kbb[intval($id3)] as $k => $v) {
 	  if ($v) {
 	    echo $k. "=>" . $v. "<br/>";
 	  }
 	}
-	echo "</td><td>";
+	echo "</td><td class='craiglist'>";
 	foreach($craiglist[intval($id2)] as $k => $v) {
 	  if ($v) {
 	    echo $k. "=>" . $v. "<br/>";
 	  }
 	}
+	if (! isset($gold[$id1])){
+	  $gold[$id1] = 'MAYBE';
+	}
 	echo "</td>";
-	echo "<td><input class='match' id='{$id1}' type='button' value='MAYBE' onclick='Matched(this)' /></td>";
+	echo "<td><input class='match' id='{$id1}' type='button' value='{$gold[$id1]}' onclick='Matched(this)' /></td>";
 	echo "</tr>";
       }
     }
