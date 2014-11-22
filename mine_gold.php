b93742bd23f7569fb86873b4edbf9f19e380b47c
Rahul Chatterjee <rchatterjee4@wisc.edu>
2014-11-22 05:59:08 -0600

    gold is working

--- mine_gold.php
+++ mine_gold.php
@@ -1,41 +1,105 @@
 <?php
-$myfile = file("data/blocking_data/blocks.table") or die("Unable to open file!");
-$total_sample = 0;
-$kbb_t = json_decode(file_get_contents("data/all_makes_table_kbb.json"), true)['table']['tuples'] or die("Cannot load kbb data");
-$craiglist_t = json_decode(file_get_contents("data/all_makes_table_craiglist.json"), true)['table']['tuples'];
-$kbb = array();
-$craiglist = array();
-
-foreach($kbb_t as $t ){
-  $kbb[$t['id']] = $t;
-}
-foreach($craiglist_t as $t ){
-  $craiglist[$t['id']] = $t;
-}
-
-echo "<table border='1'>";
-foreach ($myfile as $line_num => $line) {
-  if ($line_num ==0) continue;
-  if (rand(0, 900) == 0) {
-    echo "<tr>";
-    $total_sample += 1 ;
-    #echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
-    list($id1, $id2, $id3) = split(',', $line);
-    echo "<td>";
-    foreach($kbb[intval($id3)] as $k => $v) {
-      if ($v) {
-	echo $k. "=>" . $v. "<br/>";
-      }
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $gold_file = "data/golden_data.json";
+  $gold = json_decode(file_get_contents($gold_file), true);
+  foreach($_POST as $k => $v) {
+    if ($gold[$k] and $gold[$k] != $v) {
+      echo "Watch {$k}, {$v}, {$gold[$k]}";
     }
-    echo "</td><td>";
-    foreach($craiglist[intval($id2)] as $k => $v) {
-      if ($v) {
-	echo $k. "=>" . $v. "<br/>";
-      }
-    }
-    echo "</td></tr>";
+    $gold[$k] = $v;
   }
+  $json_string = json_encode($gold, JSON_PRETTY_PRINT);
+  file_put_contents($gold_file, $json_string);
+  echo $json_string;
+  exit();
 }
-echo "</table>";
-echo "<h1>{$total_sample}</h1>";
 ?>
+<!DOCTYPE html>
+<meta charset='UTF-8'>
+<title>Golden data</title>
+<html>
+  <header> <h2>Randomly generated ids from KBB and Craiglist to match</h2></header>
+  <script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+  <body>
+    <?php    
+    $myfile = file("data/blocking_data/blocks.table") or die("Unable to open file!");
+    $total_sample = 0;
+    $kbb_t = json_decode(file_get_contents("data/all_makes_table_kbb.json"), true)['table']['tuples'] or die("Cannot load kbb data");
+    $craiglist_t = json_decode(file_get_contents("data/all_makes_table_craiglist.json"), true)['table']['tuples'];
+    $kbb = array();
+    $craiglist = array();
+    
+    foreach($kbb_t as $t ){
+      $kbb[$t['id']] = $t;
+    }
+    foreach($craiglist_t as $t ){
+      $craiglist[$t['id']] = $t;
+    }
+
+    echo "<table border='1'>";
+    echo "<tr class='header'><td>KBB</td><td>Craiglist</td><td>Match</td></tr>";
+    foreach ($myfile as $line_num => $line) {
+      if ($line_num ==0) continue;
+      if (rand(0, 90000) == 0) {
+	echo "<tr>";
+	$total_sample += 1 ;
+	#echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
+	list($id1, $id2, $id3) = split(',', $line);
+	echo "<td>";
+	# selected.append($id1);
+	foreach($kbb[intval($id3)] as $k => $v) {
+	  if ($v) {
+	    echo $k. "=>" . $v. "<br/>";
+	  }
+	}
+	echo "</td><td>";
+	foreach($craiglist[intval($id2)] as $k => $v) {
+	  if ($v) {
+	    echo $k. "=>" . $v. "<br/>";
+	  }
+	}
+	echo "</td>";
+	echo "<td><input class='match' id='{$id1}' type='button' value='MAYBE' onclick='Matched(this)' /></td>";
+	echo "</tr>";
+      }
+    }
+    echo "</table>";
+    echo "<h1>{$total_sample}</h1>";
+    ?>
+    <div align="center">
+      <input id='submit' type='button' value="Submit all matched data" onclick="Submit()"/>
+      <script>
+       // Main data array id => Yes/No, Maybe
+       M = {};
+
+       Rotate = {"MAYBE": "YES", "YES": "NO", "NO": "MAYBE"};
+
+       function Matched(event) {
+	 if ($.isEmptyObject(M)) {
+	   console.log("Initializing M");
+	   // first initialize the M
+	   all = $('.match');
+	   for( var k in  all) {
+	     M[all[k].id] = all[k].value;
+	   }
+	   console.log(M);
+	 } 
+	 id = event.id;
+	 s = "";
+	 nVal = Rotate[event.value];
+	 event.value = nVal;
+	 M[id] = nVal;
+       }
+
+       function Submit() {
+	 $.post("mine_gold.php", M, function(returnedData) {
+	   // This callback is executed if the post was successful 
+	   alert("SUCCESS!!");
+	   console.log("Returned Value:", returnedData);
+	 }, "json");
+       }   
+      </script>
+      <footer align="center">All rights reserverd to Rahul and Saikat !</footer>
+    </div>
+  </body>
+</html>
