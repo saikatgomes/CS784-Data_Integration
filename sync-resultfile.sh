8864f42f2968c859d11431fe68b4dc8cffc2f216
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-11-26 17:38:26 -0600

    Added syncing to git script

--- sync-resultfile.sh
+++ sync-resultfile.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
 source $HOME/.bashrc
+if [ -e ../dataintegration/ ];
+then
+    echo "I am here";
+else
+    echo "Sorry take to the directory where the script is"
+fi
 
-git add gdata/result.txt
-git commit -m "AUTO COMMIT: result.txt"
-git pull
-git push origin master
+#cd $HOME/Acads/DataModels_784/dataintegration/;
+
+if [ "$1" = "push" ]
+then
+    echo "Going to push"
+    while [ 1 ];
+    do 
+	git add gdata/result.txt
+	git commit -m "AUTO COMMIT: result.txt"
+	git pull
+	git push origin master
+	sleep 5;
+    done
+elif [ "$1" = "pull" ]
+then
+    echo "I will pull only"
+    while [ 1 ];
+    do 
+	git pull
+    done
+else
+    echo "Sorry command not recognised"
+fi
+
 
